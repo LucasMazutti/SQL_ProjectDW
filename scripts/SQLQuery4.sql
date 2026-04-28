4c2ad5825dd8d54d2db37b3d9279208a61c663fc
Lucas Mazutti <lucasmazutti10@gmail.com>
2026-04-28 20:13:51 -0300

    Add files via upload

--- scripts/SQLQuery4.sql
+++ scripts/SQLQuery4.sql
@@ -1,6 +1,9 @@
--- CRIA«√O DAS TABELAS COM BASE NOS ARQUIVOS source_crm E source_erp
+-- CRIA√á√ÉO DAS TABELAS COM BASE NOS ARQUIVOS source_crm E source_erp
 
 -- CRM
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL 
+	DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
 cst_id              INT,
 cst_key             NVARCHAR(50),
@@ -11,6 +14,8 @@ cst_gndr             NVARCHAR(50),
 cst_create_date      DATE
 );
 
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL 
+	DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info(
 	prd_id          INT,
 	prd_key         NVARCHAR (50),
@@ -21,6 +26,8 @@ CREATE TABLE bronze.crm_prd_info(
 	prd_end_date    DATETIME
 );
 
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL 
+	DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num     NVARCHAR (50),
 	sls_prd_key     NVARCHAR (50),
@@ -34,23 +41,28 @@ CREATE TABLE bronze.crm_sales_details (
 );
 
 -- ERP 
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL 
+	DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12(
 	cid         NVARCHAR (50),
 	bdate       DATE,
 	gen         NVARCHAR
 );
 
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL 
+	DROP TABLE bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101(
 	cid         NVARCHAR(50),
 	entry       NVARCHAR(50)
 );
 
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL 
+	DROP TABLE bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2(
 	id          NVARCHAR(50),
 	cat         NVARCHAR(50),
 	subcat      NVARCHAR(50),
 	maintenance NVARCHAR(50)
 );
-
 
 
