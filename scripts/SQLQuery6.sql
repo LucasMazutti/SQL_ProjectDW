ae03ce01f0f85d349ae6c9419d92f7b67a1eb6e8
Lucas Mazutti <lucasmazutti10@gmail.com>
2026-05-04 20:06:45 -0300

    Add files via upload

---
+++ scripts/SQLQuery6.sql
@@ -0,0 +1,9 @@
+-- Analisando cada tabela para desenhar as relações e deixar encaminhada os joins entre elas
+
+select top 1000 * from bronze.crm_cust_info
+select top 1000 * from bronze.crm_prd_info
+select top 1000 * from bronze.crm_sales_details
+
+select top 1000 * from bronze.erp_cust_az12
+select top 1000 * from bronze.erp_loc_a101
+select top 1000 * from bronze.erp_px_cat_g1v2
