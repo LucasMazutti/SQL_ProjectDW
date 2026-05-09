9a3af68feb86c816a2831ddff2bb98bfe6e7450f
Lucas Mazutti <lucasmazutti10@gmail.com>
2026-05-09 14:54:46 -0300

    Add files via upload

---
+++ scripts/SQLQuery12.sql
@@ -0,0 +1,35 @@
+-- Tratando tabela az12
+
+INSERT INTO silver.erp_cust_az12(
+	cid,
+	bdate,
+	gen
+)
+SELECT 
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) 
+	ELSE cid
+	END AS cid,
+CASE WHEN bdate > GETDATE() THEN NULL
+	ELSE bdate
+	END AS bdate,
+CASE WHEN UPPER(TRIM(gen)) = 'M' THEN 'Male' 
+	WHEN UPPER(TRIM(gen)) = 'F' THEN 'Female'
+	WHEN UPPER(TRIM(gen)) = '' THEN 'n/a'
+	WHEN gen IS NULL THEN 'n/a'
+	ELSE gen
+END AS gen
+FROM bronze.erp_cust_az12
+
+-- validações
+
+select *
+from bronze.erp_cust_az12
+WHERE bdate > getdate()
+
+SELECT DISTINCT 
+gen
+FROM bronze.erp_cust_az12
+
+
+
+
